3ed585a5fa45237209c128e49d166ab30db5af56
Jean-Francois Jung <pedagogie@itechsup.fr>
2016-10-05 09:16:45 +0200

    Ajout de l'addition

--- Calculatrice/Program.cs
+++ Calculatrice/Program.cs
@@ -8,6 +8,27 @@ namespace Calculatrice
 {
     class Program
     {
+        static float SaisirFloat(string prompt)
+        {
+            string reponse;
+            Console.WriteLine(prompt);
+            reponse = Console.ReadLine();
+            return float.Parse(reponse);
+        }
+
+        static void Additioner()
+        {
+            float nombre1;
+            float nombre2;
+            float somme;
+
+            nombre1 = SaisirFloat("Saisie premier nombre");
+            nombre2 = SaisirFloat("Saisie second nombre");
+
+            somme = nombre1 + nombre2;
+            Console.WriteLine("La somme vaut : " + somme.ToString());
+        }
+
         static void Main(string[] args)
         {
         }
